156be92ae72fdbb6daae1d12d68460b7840f010f
dannguyen8189 <dannguyen8189@gmail.com>
2023-02-02 14:12:30 -0800

    added last 2 orgs

--- main.tf
+++ main.tf
@@ -21,3 +21,19 @@ module "app3" {
     azure_domain = "@zuoracloudeng.onmicrosoft.com"
     organization_name = "zuora-billing"
 }
+
+module "app4" {
+    source  = "./enterprise_app"
+    //version = "1.0.0"
+    users_file_path = "./users.csv"
+    azure_domain = "@zuoracloudeng.onmicrosoft.com"
+    organization_name = "zuora-revenue-core"
+}
+
+module "app5" {
+    source  = "./enterprise_app"
+    //version = "1.0.0"
+    users_file_path = "./users.csv"
+    azure_domain = "@zuoracloudeng.onmicrosoft.com"
+    organization_name = "zuora-zcloud"
+}
