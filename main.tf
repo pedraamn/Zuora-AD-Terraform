0e6f42c41c55e9340c04f796ae693f4f7390af22
Pedraam Nikzad <pedraamn@gmail.com>
2023-02-16 13:20:44 -0800

    remove redundant vars

--- main.tf
+++ main.tf
@@ -1,45 +1,25 @@
 module "app1" {
     source  = "./enterprise_app"
-    //version = "1.0.0"
-    users_file_path = "./csvs/users.csv"
-    managers_file_path = "./csvs/managers.csv"
-    azure_domain = "@zuoracloudeng.onmicrosoft.com"
     organization_name = "zuora-platform"
 }
 
 module "app2" {
     source  = "./enterprise_app"
-    //version = "1.0.0"
-    users_file_path = "./csvs/users.csv"
-    managers_file_path = "./csvs/managers.csv"
-    azure_domain = "@zuoracloudeng.onmicrosoft.com"
     organization_name = "zuora-collect"
 }
 
 module "app3" {
     source  = "./enterprise_app"
-    //version = "1.0.0"
-    users_file_path = "./csvs/users.csv"
-    managers_file_path = "./csvs/managers.csv"
-    azure_domain = "@zuoracloudeng.onmicrosoft.com"
     organization_name = "zuora-billing"
 }
 
 module "app4" {
     source  = "./enterprise_app"
-    //version = "1.0.0"
-    users_file_path = "./csvs/users.csv"
-    managers_file_path = "./csvs/managers.csv"
-    azure_domain = "@zuoracloudeng.onmicrosoft.com"
     organization_name = "zuora-revenue-core"
 }
 
 module "app5" {
     source  = "./enterprise_app"
-    //version = "1.0.0"
-    users_file_path = "./csvs/users.csv"
-    managers_file_path = "./csvs/managers.csv"
-    azure_domain = "@zuoracloudeng.onmicrosoft.com"
     organization_name = "zuora-zcloud"
 }
 
